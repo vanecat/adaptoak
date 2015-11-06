5b9a36b8a1bd8f622f4bbbcdc4d7c65f8e16ef02
johnvf <john.victor.faichney@gmail.com>
2015-11-06 13:14:18 -0800

    working vector layer using mapboxgl

--- client/scripts/directives/Map.js
+++ client/scripts/directives/Map.js
@@ -1,5 +1,5 @@
 'use strict';
-var L = require('mapbox.js');
+var mapboxgl = require('mapbox-gl');
 
 exports.inject = function(app) {
   app.directive('map', exports.directive);
@@ -16,99 +16,51 @@ exports.directive = function() {
     template: '<ul id="map-ui"></ul><div id="map" class="leaflet-container leaflet-retina leaflet-fade-anim" tabindex="0"></div>',
     link: function(scope, element, attrs, ctrl) {
 
-        L.mapbox.accessToken = 'pk.eyJ1IjoidWJtYXBzIiwiYSI6Il9EUThaMUEifQ.45ITMBnO4PJfE0fCD-Bxpw'
+      mapboxgl.accessToken = 'pk.eyJ1Ijoiam9obnZmIiwiYSI6IjFkZGY0OTk4Mjg5MDU0ZjNiYmU4YWFjODg2YzQ0ZTk2In0.cUp8RaZxpmq7A7KGVNucKQ';
 
-        var southWest = L.latLng(37.581209, -122.498682),
-          northEast = L.latLng(37.897507, -122.080184),
-          bounds = L.latLngBounds(southWest, northEast);
-          
-        var map = L.mapbox.map('map', 'ubmaps.map-kb224le4', { 
-          zoomControl: false,
-          minZoom: 12,
-          maxZoom: 19,
-          maxBounds: bounds
-        })
-          
-          .setView([37.8119,-122.3028], 15)
+      var map = new mapboxgl.Map({
+          container: 'map', // container id
+          style: 'mapbox://styles/mapbox/streets-v8', //stylesheet location
+          center: [ -122.3028, 37.8119], // starting position
+          zoom: 15 // starting zoom
+      });
 
-        var ui = document.getElementById('map-ui');
-        //I put this here    
-        var thelayer = null;
+      map.on('style.load', function () {
 
-        L.control.layers({
-          'Streets' : L.mapbox.tileLayer('ubmaps.map-kb224le4')
-        },{collapsed: false, position: 'topleft'}).addTo(map);
-          
-        var AO = L.mapbox.tileLayer('ubmaps.hd9mfekb');
-        var Caltrans = L.mapbox.tileLayer('ubmaps.ha733p25');
-        var Caltransbuffer = L.mapbox.tileLayer('ubmaps.hd0pj88j');
-        var TruckBuffer = L.mapbox.tileLayer('ubmaps.hpo8nk4n');
-        var Parcel500 = L.mapbox.tileLayer('ubmaps.hnb19lk5');
-        var baaqmd = L.mapbox.tileLayer('ubmaps.hgd2f95d');
-        var oaklandowner = L.mapbox.tileLayer('ubmaps.hnb26i58');
-        var zoning = L.mapbox.tileLayer('ubmaps.h6b0lp81');
-        var baytrail = L.mapbox.tileLayer('ubmaps.BayTrail');
-        var envirostor = L.mapbox.tileLayer('ubmaps.hp1e3041');
-        var forest = L.mapbox.tileLayer('ubmaps.CalTrans_Forest');    
-          
-        addLayer(AO, L.mapbox.gridLayer('ubmaps.hd9mfekb'),'Adapt Oakland Area', 11);
-        addLayer(Caltrans, L.mapbox.gridLayer('ubmaps.ha733p25'),'Caltrans Right-of-way', 9);
-        addLayer(Caltransbuffer, L.mapbox.gridLayer('ubmaps.hd0pj88j'),'500ft Freeway Setback', 8);
-        addLayer(TruckBuffer, L.mapbox.gridLayer('ubmaps.hpo8nk4n'),'500ft Truck Route Setback', 7);    
-        addLayer(baytrail, L.mapbox.gridLayer('ubmaps.BayTrail'),'Bay Trail', 12);
-          
-        addLayer(baaqmd, L.mapbox.gridLayer('ubmaps.hgd2f95d'),'Point Source Pollution', 6);
-        addLayer(envirostor, L.mapbox.gridLayer('ubmaps.hp1e3041'),'Envirostor', 13);
-        addLayer(zoning, L.mapbox.gridLayer('ubmaps.h6b0lp81'),'Zoning', 2);    
-        addLayer(oaklandowner, L.mapbox.gridLayer('ubmaps.hnb26i58'),'Public Land', 5);
-        addLayer(Parcel500, L.mapbox.gridLayer('ubmaps.hnb19lk5'),'Parcels in the 500ft Setback', 10);
-          
-        addLayer(forest, L.mapbox.gridLayer('ubmaps.CalTrans_Forest'),'CalTrans Plantable Areas', 14);
+        map.addSource('2015_parcels', {
+            type: 'vector',
+            url: 'mapbox://johnvf.cdc0d47d'
+        });
 
-          
-        function addLayer(layer, gridlayer, name, zIndex) {
-          layer
-              .setZIndex(zIndex)
+        map.addLayer({
+            "id": "2015_parcels",
+            "type": "line",
+            "source": "2015_parcels",
+            "source-layer": "oakland_public_update_2015",
+            "layout": {
+              "line-join": "round",
+              "line-cap": "round"
+            },
+            "paint": {
+              "line-color": "#ff69b4",
+              "line-width": 1,
+            }
+        });
 
-          gridlayer
-          // add the gridControl to the active gridlayer
-          var gridControl = L.mapbox.gridControl(gridlayer, {follow: false}).addTo(map);
-          
-          // Create a simple layer switcher that toggles layers on and off.
-          var item = document.createElement('li');
-          var link = document.createElement('a');
+        map.addLayer({
+            "id": "2015_parcels_fill",
+            "type": "fill",
+            "source": "2015_parcels",
+            "source-layer": "oakland_public_update_2015",
+            "paint": {
+              "fill-color": "#ff69b4",
+              "fill-opacity": 0.5
+            }
+        });
 
-          link.href = '#';
-          link.innerHTML = name;
+      });
 
-          
-          link.onclick = function(e) {
-              e.preventDefault();
-              e.stopPropagation();
-
-              if (map.hasLayer(layer)) {
-                  map.removeLayer(layer);
-                  map.removeLayer(gridlayer);
-                  map.legendControl.removeLegend(layer.getTileJSON().legend);
-                  
-                  this.className = '';
-              } else {
-                  map.addLayer(layer);
-                  map.addLayer(gridlayer);
-                  this.className = 'active';
-                  map.legendControl.addLegend(layer.getTileJSON().legend);
-
-              }
-                 thelayer = layer;  
-
-
-              
-          };
-          item.appendChild(link);
-          ui.appendChild(item);
-        }
-          
-        new L.Control.Zoom({ position: 'topleft' }).addTo(map);
     }
-  };
+
+  }
 };
